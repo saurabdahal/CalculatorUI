a308fa34328916b95260edee3f3356e063dbfa22
saurav dahal <d.saurab1@gmail.com>
2024-06-10 21:33:35 -0400

    Feat: modified calcEqualsButton to add Button

--- app/src/main/java/com/example/calculatorui/MainActivity.kt
+++ app/src/main/java/com/example/calculatorui/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculatorui.ui.theme.CalculatorUITheme
@@ -27,4 +29,32 @@ fun CalcView(){
 
 }
 
+@Composable
+fun CalcRow(){
 
+}
+
+@Composable
+fun CalcDisplay() {
+
+}
+
+@Composable
+fun CalcNumericButton() {
+
+}
+
+@Composable
+fun CalcOperationButton() {
+
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=")
+    }
+}
