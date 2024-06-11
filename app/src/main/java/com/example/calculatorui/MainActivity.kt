c54bf01eda258519639991b557bbbcbb4aa18d48
saurav dahal <d.saurab1@gmail.com>
2024-06-10 21:36:33 -0400

    feat: modified calcOperationButton to add operation and dispaly parameter

--- app/src/main/java/com/example/calculatorui/MainActivity.kt
+++ app/src/main/java/com/example/calculatorui/MainActivity.kt
@@ -45,8 +45,13 @@ fun CalcNumericButton() {
 }
 
 @Composable
-fun CalcOperationButton() {
-
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+    Button(
+        onClick = {},
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = operation)
+    }
 }
 
 @Composable
